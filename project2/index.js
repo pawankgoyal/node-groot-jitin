4e2627282bd6f25e4df7efc25bca1118640a0a47
pawankgoyal <goyalpawan357@gmail.com>
2023-09-25 20:19:45 +0530

    delete api

--- project2/index.js
+++ project2/index.js
@@ -2,14 +2,21 @@ const express = require('express');
 const app = express();
 
 const PORT = 1200;
-app.use(express.json());
+app.use(express.json())
 
 let products = [
     {
+        id: 1,
         title: 'shoes',
         price: 1200
     },
     {
+        id: 2,
+        title: 'shoes',
+        price: 120
+    },
+    {
+        id: 3,
         title: 'shirt',
         price: 450
     }
@@ -20,10 +27,28 @@ app.get('/product', (req, res) => {
 });
 
 app.post('/product', (req, res) => {
-    // console.log(req.body)
+    console.log(req.body)
     products.push(req.body)
     res.send(products)
 })
+
+app.delete('/product/:id', (req, res) => {
+    const id = req.params.id
+    // products.map((e, i) => {
+    //     if (e.id == req.params.id) {
+    //         products.splice(i, 1);
+    //     }
+    // })
+
+    const index = products.findIndex(e => e.id == id);
+
+    if (index == -1) {
+        return res.send({ message: 'product does not exist' })
+    }
+    products.splice(index, 1);
+    res.send(products);
+})
+
 
 app.listen(PORT, (err) => {
     if (err) {
