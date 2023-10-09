a1f1fd228bb03ca4b2b333b25e50a2215fc9d17a
swamiankit261 <swamiankit261@gmail.com>
2023-10-09 20:33:39 +0530

    get, delete

--- project3-(mongodb)/index.js
+++ project3-(mongodb)/index.js
@@ -3,17 +3,28 @@ const app = express();
 const port = 1400;
 const mongoose = require('mongoose');
 const product = require('./product');
+app.use(express.json())
 
-mongoose.connect('mongodb://localhost:27017/groot-node')
+mongoose.connect('mongodb://127.0.0.1:27017/groot-node?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.0.1')
     .then(() => console.log("db connected"))
     .catch((err) => console.log(err))
 
-app.get('/', async (req, res) => {
-    product.create({
-        title: "shoes",
-        price: 12
-    })
+app.post('/', async (req, res) => {
+    console.log(req.body)
+    product.create(req.body)
+        .catch(err => console.log(err))
+
     res.send("done")
+})
+
+app.get('/', async (req, res) => {
+    let data = await product.find({})
+    res.send(data)
+})
+
+app.delete('/:id', async (req, res) => {
+    await product.findByIdAndDelete(req.params.id)
+    res.send('done')
 })
 
 app.listen(port, (err) => {
