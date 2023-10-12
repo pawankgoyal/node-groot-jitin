f6f42af6e6dbdd07d0726da83316e2e293f899b9
pawankgoyal <goyalpawan357@gmail.com>
2023-10-12 20:27:06 +0530

    payload validation and password hashing

--- project4-(auth)/index.js
+++ project4-(auth)/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const connectDb = require('./dbConnect');
 const User = require('./models/User')
+const bcrypt = require('bcrypt')
+
 connectDb()
 const app = express()
 app.use(express.json())
@@ -9,17 +11,30 @@ const PORT = 1500;
 
 app.post('/register', async (req, res) => {
     try {
+        const { name, email, password } = req.body
 
+        if (!name) {
+            return res.send({ err: "name is required" })
+        } else if (!email) {
+            return res.send({ err: "email is required" })
+        } else if (!password) {
+            return res.send({ err: "password is required" })
+        }
 
         const user = await User.findOne({ email: req.body.email });
-        console.log(user)
+
+        const salt = bcrypt.genSaltSync(10)
+        const hashPassword = bcrypt.hashSync(password, salt)
+
+        req.body.password = hashPassword;
+
         // null, undefined , 0 => false
         if (user) {
             return res.send({ err: "user already exists" })
         }
 
-        await User.create(req.body)
-        res.send({ message: 'registered successfully' })
+        let response = await User.create(req.body)
+        res.send(response)
     } catch (error) {
         res.send({ err: error })
     }
