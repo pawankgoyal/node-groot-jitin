e94454018f77ccff451b1e5a50cd54b1640e5d49
pawankgoyal <goyalpawan357@gmail.com>
2023-10-16 20:37:10 +0530

    express validator

--- project4-(auth)/index.js
+++ project4-(auth)/index.js
@@ -1,25 +1,37 @@
 const express = require('express');
 const connectDb = require('./dbConnect');
 const User = require('./models/User')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
+const { validationResult, body } = require('express-validator');
 
 connectDb()
 const app = express()
 app.use(express.json())
 const PORT = 1500;
 
+const registerValidations = [
+    body('name', "Name must be have 2 chars").isLength({ min: 2 }),
+    body('email', "Invalid email").isEmail(),
+    body('password', "password must be strong").isStrongPassword(),
+]
 
-app.post('/register', async (req, res) => {
+app.post('/register', registerValidations, async (req, res) => {
     try {
+        const result = validationResult(req);
+        if (!result.isEmpty()) {
+            return res.send({ errors: result.array() });
+        }
+
         const { name, email, password } = req.body
 
-        if (!name) {
-            return res.send({ err: "name is required" })
-        } else if (!email) {
-            return res.send({ err: "email is required" })
-        } else if (!password) {
-            return res.send({ err: "password is required" })
-        }
+        // if (!name) {
+        //     return res.send({ err: "name is required" })
+        // } else if (!email) {
+        //     return res.send({ err: "email is required" })
+        // } else if (!password) {
+        //     return res.send({ err: "password is required" })
+        // }
+
 
         const user = await User.findOne({ email: req.body.email });
 
@@ -33,10 +45,15 @@ app.post('/register', async (req, res) => {
             return res.send({ err: "user already exists" })
         }
 
-        let response = await User.create(req.body)
-        res.send(response)
+        let response = await User.create(req.body);
+        let data = JSON.parse(JSON.stringify(response))
+        delete data.password
+
+        console.log(data)
+        res.send(data)
     } catch (error) {
-        res.send({ err: error })
+        console.log(error)
+        res.send({ err: error.message })
     }
 
 })
